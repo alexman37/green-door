53f1e317e60041658d608177524e5e8f8c44cc1e
aack37 <alexackler@gmail.com>
2025-10-13 21:00:00 -0400

    Scripted events basic implementation
    Some example buttons on the right show how scripted events can be used

--- Assets/ScriptedTimedEvent.cs
+++ Assets/ScriptedTimedEvent.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 /* See ScriptedEvent. It's the same idea, but these are Coroutines instead of methods.
  * All events run in order and take a certain amount of time to complete
@@ -33,6 +34,15 @@ public class ScriptedTimedEvent : MonoBehaviour
         {
             case ScriptedEventType.MOVEMENT:
                 coroutine = movementLinearCoroutine(inputs.focusObject, inputs.vector, inputs.time);
+                break;
+            case ScriptedEventType.MOVEMENT_X:
+                coroutine = movementLinearCoroutine(inputs.focusObject, inputs.vector, inputs.time);
+                break;
+            case ScriptedEventType.ENABLE:
+                coroutine = enableCoroutine(inputs.focusObject, inputs.flag);
+                break;
+            case ScriptedEventType.FADE:
+                coroutine = fadeImageCoroutine(inputs.focusObject, inputs.flag, inputs.time);
                 break;
         }
     }
@@ -68,9 +78,43 @@ public class ScriptedTimedEvent : MonoBehaviour
         Vector3 startPos = focus.transform.position;
         for(float t = 0; t < time; t += Time.deltaTime)
         {
-            focus.transform.position = Vector3.Lerp(startPos, startPos + position, t / time);
+            focus.transform.position = Vector3.Lerp(startPos, startPos + position, Mathf.Clamp(t / time, 0, 1));
             yield return null;
         }
+        focus.transform.position = startPos + position;
+        EventManager.instance.finishEventExecution();
+    }
+
+    public static IEnumerator movementExponentialCoroutine(GameObject focus, Vector3 position, float time)
+    {
+        Vector3 startPos = focus.transform.position;
+        for (float t = 0; t <= time; t += Time.deltaTime)
+        {
+            focus.transform.position = Utils.XerpStandard(startPos, startPos + position, Mathf.Clamp(t / time, 0, 1));
+            yield return null;
+        }
+        focus.transform.position = startPos + position;
+        EventManager.instance.finishEventExecution();
+    }
+
+    public static IEnumerator enableCoroutine(GameObject focus, bool enabled)
+    {
+        focus.SetActive(enabled);
+        yield return null;
+        EventManager.instance.finishEventExecution();
+    }
+
+    public static IEnumerator fadeImageCoroutine(GameObject focus, bool fadingIn, float time)
+    {
+        Image img = focus.GetComponent<Image>();
+        Color col = img.color;
+        for (float t = 0; t <= time; t += Time.deltaTime)
+        {
+            img.color = new Color(col.r, col.g, col.b, (fadingIn ? (t / time) : 1 - (t / time)));
+            Debug.Log("New fade col " + img.color);
+            yield return null;
+        }
+        img.color = new Color(col.r, col.g, col.b, (fadingIn ? 1 : 0));
         EventManager.instance.finishEventExecution();
     }
 }
@@ -82,9 +126,15 @@ public class ScriptedEventInputs
     public GameObject focusObject;
     public Vector3 vector;
     public float time;
+    public bool flag;
+    public Color col;
 }
 
 public enum ScriptedEventType
 {
-    MOVEMENT
+    MOVEMENT,
+    MOVEMENT_X,
+    ENABLE,
+    FADE,
+    ANIMATE,
 }
